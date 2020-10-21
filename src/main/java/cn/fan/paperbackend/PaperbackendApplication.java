014e95e0a807a37ca14805c333f2f255e22e78e3
lirongfan <lirongfan>
2020-10-21 10:25:35 +0800

    Initial commit

---
+++ src/main/java/cn/fan/paperbackend/PaperbackendApplication.java
@@ -0,0 +1,13 @@
+package cn.fan.paperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fan/paperbackend/PaperbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fan.paperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
